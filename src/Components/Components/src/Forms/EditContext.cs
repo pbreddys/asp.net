0c6e25b9386e97a92f0f6536881d7750771e1856
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2019-02-08 14:50:16 +0000

    Try removing EditScope

--- src/Components/Components/src/Forms/EditContext.cs
+++ src/Components/Components/src/Forms/EditContext.cs
@@ -12,44 +12,36 @@ namespace Microsoft.AspNetCore.Components.Forms
     /// </summary>
     public class EditContext
     {
-        private Dictionary<FieldIdentifier, Dictionary<EditScope, FieldState>> _statesByField = new Dictionary<FieldIdentifier, Dictionary<EditScope, FieldState>>();
-        private Dictionary<EditScope, Dictionary<FieldIdentifier, FieldState>> _statesByScope = new Dictionary<EditScope, Dictionary<FieldIdentifier, FieldState>>();
-        private EditScope _modificationTracker;
+        private Dictionary<FieldIdentifier, FieldState> _fieldStates = new Dictionary<FieldIdentifier, FieldState>();
 
         public EditContext(object model)
         {
             Model = model ?? throw new ArgumentNullException(nameof(model));
-            _modificationTracker = CreateScope();
         }
 
-        public object Model { get; }
+        public object Model { get; } // TODO: Is this really needed?
 
         public event EventHandler<FieldIdentifier> FieldChanged;
 
         public event EventHandler ValidationRequested;
 
-        /// <summary>
-        /// Creates an <see cref="EditScope"/> that can be used to get or set state information for fields within this <see cref="EditContext"/>.
-        /// </summary>
-        /// <returns>The newly-created <see cref="EditScope"/>.</returns>
-        public EditScope CreateScope()
-            => new EditScope(this);
+        public ValidationSource CreateValidationSource()
+            => new ValidationSource(this);
 
         public bool IsModified()
-            => _statesByField.Any(statesByFieldEntry => statesByFieldEntry.Value.Any(state => state.Value.IsModified));
+            => _fieldStates.Values.Any(s => s.IsModified);
 
         public bool IsModified(FieldIdentifier fieldIdentifier)
-            => _statesByField.TryGetValue(fieldIdentifier, out var statesByFieldEntry)
-            ? statesByFieldEntry.Values.Any(state => state.IsModified)
+            => _fieldStates.TryGetValue(fieldIdentifier, out var statesByFieldEntry)
+            ? statesByFieldEntry.IsModified
             : false;
 
         public void ClearModifications()
         {
-            // This is a bit odd, because if any other EditScope has set flags, then this "clear"
-            // method won't do anything. Maybe it shouldn't be native to FieldState, but rather is
-            // either some external propertybag-based mechanism, or is just native to the EditContext
-            // itself independently of the edit scopes.
-            _modificationTracker.Clear();
+            foreach (var fieldState in _fieldStates.Values)
+            {
+                fieldState.IsModified = false;
+            }
         }
 
         public bool IsValid()
@@ -59,16 +51,16 @@ namespace Microsoft.AspNetCore.Components.Forms
             => !ValidationErrorMessages(fieldIdentifier).Any();
 
         public IEnumerable<string> ValidationErrorMessages()
-            => _statesByField.Values.SelectMany(pair => pair.Values).SelectMany(state => state.ValidationErrorMessages);
+            => _fieldStates.Values.SelectMany(state => state.ValidationErrorMessages);
 
         public IEnumerable<string> ValidationErrorMessages(FieldIdentifier fieldIdentifier)
-            => _statesByField.TryGetValue(fieldIdentifier, out var statesByFieldEntry)
-            ? statesByFieldEntry.Values.SelectMany(state => state.ValidationErrorMessages)
+            => _fieldStates.TryGetValue(fieldIdentifier, out var statesByFieldEntry)
+            ? statesByFieldEntry.ValidationErrorMessages
             : Enumerable.Empty<string>();
-
+        
         public void NotifyFieldChanged(FieldIdentifier fieldIdentifier)
         {
-            _modificationTracker[fieldIdentifier].IsModified = true;
+            GetState(fieldIdentifier, true).IsModified = true;
             FieldChanged?.Invoke(this, fieldIdentifier);
         }
 
@@ -78,94 +70,21 @@ namespace Microsoft.AspNetCore.Components.Forms
             return IsValid();
         }
 
-        internal FieldState GetState(EditScope scope, FieldIdentifier identifier)
-            => _statesByScope.TryGetValue(scope, out var statesByScopeEntry)
-            && statesByScopeEntry.TryGetValue(identifier, out var result)
-            ? result
-            : null;
-
-        internal void SetState(EditScope scope, FieldIdentifier fieldIdentifier, FieldState state)
+        internal FieldState GetState(FieldIdentifier identifier, bool ensureExists)
         {
-            if (scope == null)
+            if (_fieldStates.TryGetValue(identifier, out var result))
             {
-                throw new System.ArgumentNullException(nameof(scope));
+                return result;
             }
-
-            if (state == null)
+            else if (ensureExists)
             {
-                throw new System.ArgumentNullException(nameof(state));
+                result = new FieldState();
+                _fieldStates.Add(identifier, result);
+                return result;
             }
-
-            if (!_statesByField.TryGetValue(fieldIdentifier, out var statesByFieldEntry))
+            else
             {
-                statesByFieldEntry = new Dictionary<EditScope, FieldState>();
-                _statesByField.Add(fieldIdentifier, statesByFieldEntry);
-            }
-
-            if (!_statesByScope.TryGetValue(scope, out var statesByScopeEntry))
-            {
-                statesByScopeEntry = new Dictionary<FieldIdentifier, FieldState>();
-                _statesByScope.Add(scope, statesByScopeEntry);
-            }
-
-            statesByFieldEntry[scope] = state;
-            statesByScopeEntry[fieldIdentifier] = state;
-        }
-
-        internal void ClearState(EditScope scope, FieldIdentifier fieldIdentifier)
-        {
-            if (scope == null)
-            {
-                throw new System.ArgumentNullException(nameof(scope));
-            }
-
-            ClearStatesByFieldEntry(scope, fieldIdentifier);
-            ClearStatesByScopeEntry(scope, fieldIdentifier);
-        }
-
-        internal void ClearState(EditScope scope)
-        {
-            if (scope == null)
-            {
-                throw new System.ArgumentNullException(nameof(scope));
-            }
-
-            if (_statesByScope.TryGetValue(scope, out var statesByScopeEntry))
-            {
-                _statesByScope.Remove(scope);
-
-                // The entry we just removed tells us which fields there are corresponding states for,
-                // so we don't have to iterate through the complete list of fields
-                foreach (var fieldIdentifier in statesByScopeEntry.Keys)
-                {
-                    ClearStatesByFieldEntry(scope, fieldIdentifier);
-                }
-            }
-        }
-
-        private void ClearStatesByFieldEntry(EditScope scope, FieldIdentifier fieldIdentifier)
-        {
-            if (_statesByField.TryGetValue(fieldIdentifier, out var statesByFieldIdentifier))
-            {
-                statesByFieldIdentifier.Remove(scope);
-
-                if (statesByFieldIdentifier.Count == 0)
-                {
-                    _statesByField.Remove(fieldIdentifier);
-                }
-            }
-        }
-
-        private void ClearStatesByScopeEntry(EditScope scope, FieldIdentifier fieldIdentifier)
-        {
-            if (_statesByScope.TryGetValue(scope, out var statesByScopeEntry))
-            {
-                statesByScopeEntry.Remove(fieldIdentifier);
-
-                if (statesByScopeEntry.Count == 0)
-                {
-                    _statesByScope.Remove(scope);
-                }
+                return null;
             }
         }
     }
--- src/Components/Components/src/Forms/FieldState.cs
+++ src/Components/Components/src/Forms/FieldState.cs
@@ -2,14 +2,41 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Microsoft.AspNetCore.Components.Forms
 {
-    public class FieldState
+    internal class FieldState
     {
-        public List<string> ValidationErrorMessages { get; } = new List<string>();
+        private Dictionary<ValidationSource, List<string>> _validationErrorMessages { get; } = new Dictionary<ValidationSource, List<string>>(); // TODO: Instantiate lazily
 
-        public bool IsModified;
+        public bool IsModified { get; set; }
+
+        public IEnumerable<string> ValidationErrorMessages
+            => _validationErrorMessages.Values.SelectMany(x => x);
+
+        public List<string> GetValidationErrorMessagesForSource(ValidationSource source, bool ensureExists)
+        {
+            if (_validationErrorMessages.TryGetValue(source, out var result))
+            {
+                return result;
+            }
+            else if (ensureExists)
+            {
+                result = new List<string>();
+                _validationErrorMessages.Add(source, result);
+                return result;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public void ClearValidationErrorMessagesForSource(ValidationSource source)
+        {
+            _validationErrorMessages.Remove(source);
+        }
 
         // Consider also additional a general property bag here, with the ability for EditContext
         // to be able to query across all field states for a given object key. This could be used
